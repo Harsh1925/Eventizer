94a395becd8f92c34baf9fb4d0ead7925cc10f4f
Harsh Modi <d007modi@gmail.com>
2023-09-10 14:05:18 -0400

    Updated Code

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,24 @@
-export const getPosts = (req, res) => {
-    res.send("This is working too");
+import postMessage from "../models/postMessages.js";
+
+export const getPosts = async (req, res) => {
+    try {
+
+        const postMessages = await postMessage.find();
+
+        res.status(200).json(postMessages);
+
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+}
+
+export const createPost = async (req, res) => {
+    const post = req.body;
+    const newPost = new postMessage(post)
+    try {
+        await newPost.save();
+        res.status(201).json(newPost);
+    } catch (error) {
+        res.status(409).json({ message: error.message });
+    }
 }
